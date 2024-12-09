bec1d16adca1528e909a91cb1c255d0795f04a86
Sufyan Shaikh <absufyn@gmail.com>
2024-12-09 11:34:31 +0530

    contact dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,17 @@ import { JsonFormatterComponent } from './components/json-formatter/json-formatt
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  isModalOpen: boolean = false;
+
+  emailLink: string = 'mailto:absufyn@gmail.com?subject=Bug%20Report&body=Describe%20the%20issue%20here';
+  githubIssueLink: string = 'https://github.com/abu-sufyan1/json-utility/issues/new?title=Bug%20Report&body=Describe%20your%20issue%20here';
+
+  openContactModal(): void {
+    this.isModalOpen = true;
+  }
+
+  closeContactModal(): void {
+    this.isModalOpen = false;
+  }
 }
+
